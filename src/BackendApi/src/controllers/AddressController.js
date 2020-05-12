2a82db848bead8e9275495b5dd841cb28dfdc2be
Leonardo guedes <leonardo.guedes.rocha@gmail.com>
2020-05-12 20:21:57 -0300

    Web Crawler ConsultarCEP

--- src/BackendApi/src/controllers/AddressController.js
+++ src/BackendApi/src/controllers/AddressController.js
@@ -1,19 +1,19 @@
 const connection = require('../database/connection');
-
+const minhaPromise = require('../service/ConsultarCEP');
 module.exports  =   {
 
-    async index(request, response) {
-        
+    async index(request, response) {   
         const address = await connection('Address').select('*');
-
+        minhaPromise().then(function(response){
+            console.log(response);
+        })
         return response.json(address);
     },
-
   async create(request, response) {
         const { name,CEP,TypesId,CitiesId} = request.body;
         // Trazer todas a informações do ConsultarCEP para aqui
         //const address = await connection('Address').insert({name,CEP,TypesId,CitiesId});
-        response.json({ address });
+        response.json({ CEP });
     },
     async update(request , response){
         const {name,CEP,TypesId,CitiesId}= request.body;
--- src/BackendApi/src/service/ConsultarCEP.js
+++ src/BackendApi/src/service/ConsultarCEP.js
@@ -1,43 +1,50 @@
 var http = require('http');
 var qs = require('querystring');
-
-dados ={
-    "relaxation": "02871000",
-    "tipoCEP": "ALL",
-    "semelhante": "N"
-} 
-
-
-var parametros={
-    "method": "POST",
-    "hostname": "www.buscacep.correios.com.br",
-    "path":"/sistemas/buscacep/resultadoBuscaCepEndereco.cfm",
-    "headers":{
-        "content-type" : "application/x-www-form-urlencoded"
-    }
+ 
+var minhaPromise = function(){
+    return new Promise(function(resolve , reject){
+        dados ={
+            "relaxation": process.argv[2],
+            "tipoCEP": "ALL",
+            "semelhante": "N"
+        } 
+        
+        
+        var parametros={
+            "method": "POST",
+            "hostname": "www.buscacep.correios.com.br",
+            "path":"/sistemas/buscacep/resultadoBuscaCepEndereco.cfm",
+            "headers":{
+                "content-type" : "application/x-www-form-urlencoded"
+            }
+        }
+                        var req = http.request(parametros, function(httpResponse) {
+                
+                        var pedacos = []
+            
+                        httpResponse.on("data",function (pedaco) {
+                            pedacos.push(pedaco);
+                        });
+            
+                        httpResponse.on("end",function teste() {
+            
+                       var body = Buffer.concat(pedacos);
+                       var html = body.toString("latin1");
+                       var regularExpression = /(?:<td.*?>)(.*?)(?:<\/td>)/g
+        
+                        var m;
+                        var resultado = [];
+                        while ((m = regularExpression.exec(html)) !== null){
+                         resultado.push(m[1].replace("&nbsp;",""));
+                         resolve(resultado);
+                      }
+                });
+            })
+            req.write(qs.stringify(dados));
+            req.end();
+    });
 }
-
-var req = http.request(parametros, function(httpResponse) {
-        
-    var pedacos = []
-
-    httpResponse.on("data",function (pedaco) {
-        pedacos.push(pedaco);
-    });
-
-    httpResponse.on("end",function () {
-
-        var body = Buffer.concat(pedacos);
-        var html = body.toString("latin1");
-        var regularExpression = /(?:<td.*?>)(.*?)(?:<\/td>)/g
-        
-        var m;
-        var resultado = [];
-        while ((m = regularExpression.exec(html)) !== null){
-            resultado.push(m[1].replace(""));
-         }
-         console.log(resultado);
-    });
-})
-req.write(qs.stringify(dados));
-req.end();
+/*minhaPromise()
+    .then(function(response){
+        console.log(response);
+    })*/
